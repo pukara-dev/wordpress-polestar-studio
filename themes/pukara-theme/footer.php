24a12b6e877eb1fa850fe4e1512f678bc23df14a
Lina Almeida <lina@pukara.es>
2024-06-04 22:48:57 -0300

    [PPSD-10] Footer styles

--- themes/pukara-theme/footer.php
+++ themes/pukara-theme/footer.php
@@ -1,3 +1,61 @@
-		<?php wp_footer(); ?>
-	</body>
-</html>
+<?php wp_footer(); ?>
+<div class="site-footer">
+  <div class="custom__footer">
+    <div class="top__section">
+      <div class="title__container">
+        <h3 class="title">Keep Moving<br>With <span>Polestar</span></h3>
+        <a rel="canonical" href="/book-a-class/">
+          <button class="custom-button orange-button">Book a Class
+            <svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M11.2078 5.80967L7.59383 2.19568L6.40347 3.38603L9.17563 6.15819H0.757294V7.84186H9.17563L6.40347 10.614L7.59383 11.8044L11.2078 8.19038C11.5235 7.87464 11.7008 7.44647 11.7008 7.00003C11.7008 6.55358 11.5235 6.12541 11.2078 5.80967Z"
+                fill="#223146" />
+            </svg>
+          </button>
+        </a>
+      </div>
+      <div class="column__container">
+        <div class="links__container">
+          <div class="links">
+            <h4>Company</h4>
+            <ul>
+              <li><a rel="canonical" href="/about-us/">About Polestar Studio</a></li>
+              <li><a rel="canonical" href="/jobs/">Find a Job</a></li>
+              <li><a rel="canonical" href="/webinar/">Polestar Pilates Hour – Free Weekly Webinar</a></li>
+              <!-- <li><a rel="canonical" href="#">Webinar</a></li> -->
+            </ul>
+          </div>
+
+          <div class="links buttons">
+            <ul>
+              <li>
+                <a href="https://instagram.com/polestarpilates"><span class="dashicons dashicons-instagram"></span></a>
+              </li>
+              <li>
+                <a href="https://facebook.com/polestarpilates"><span class="dashicons dashicons-facebook"></span></a>
+              </li>
+              <li>
+                <a href="https://www.youtube.com/c/PolestarPilatesIntl?utm_source=website&amp;utm_medium=link"><span
+                    class="dashicons dashicons-youtube"></span></a>
+              </li>
+              <li>
+                <a href="https://twitter.com/polestarpilates"><span class="dashicons dashicons-twitter"></span></a>
+              </li>
+              <li>
+                <a href="https://mailchi.mp/polestarpilates/subscribe"><span
+                    class="dashicons dashicons-email-alt"></span></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="bottom__section">
+          <a class="cookies__link" rel="canonical" href="/privacy-policy/">Cookie Policy (EU)</a>
+          <span class="credits">© Polestar Pilates Studio 2016 - 2024</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  </body>
+
+  </html>
